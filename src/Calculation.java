29746fc3f19ef431c8528bbcaa1a87e0c41ab324
kaalid arare <jaliddd11@gmail.com>
2024-10-04 13:19:45 -0700

    commit

---
+++ src/Calculation.java
@@ -0,0 +1,18 @@
+
+
+public class Calculation {
+
+    public static void main(String[] args) {
+        System.out.println(sum(200));
+        System.out.println(mult(5));
+    }
+
+    public static int sum(int num) {
+        return num + num;
+    }
+
+    public static double mult(int num) {
+        return num * 2;
+    }
+    
+}
